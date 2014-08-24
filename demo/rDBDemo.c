651fe6884b762c49cc3e0f6c499100c0c970a940
Assaf Stoler <gh@assafstoler.com>
2014-08-24 09:32:36 -0500

    Beautifying demo code, Adding examples

--- demo/rDBDemo.c
+++ demo/rDBDemo.c
@@ -4,28 +4,6 @@
 #include <math.h>   // pow
 #include "rDB.h"
 
-#define PP_T  rdb_bpp_t
-
-// ow many Indexes we would like to keep on out demo data.
-// the use of define here is not required and done for conveniance only
-#define INDEXES 4 
-
-typedef struct DEMO_DATA {
-    // this is the rDB pointer pack (times # of indexes for this structure) 
-	PP_T	pp[INDEXES]; 
-    // below is user data
-	char 	    name[64];
-	char 	    *address_ptr;
-	uint32_t    age;
-	int64_t	    long_value;
-} demo_data_t;
-
-demo_data_t dd,
-            *pdd;
-// This os the handle which is used to access and identify a data pool
-rdb_pool_t *pool;
-
-
 #define fatal(b,arg...) do {     \
         fprintf(stderr,b,##arg); \
         fprintf(stderr,"\n");    \
@@ -45,7 +23,35 @@ rdb_pool_t *pool;
         fflush(stdout);          \
         } while (0);
 
+// On how many indexes we would refferance our demo data.
+// the use of define here is not required and done for conveniance only
+#define INDEXES 4 
 
+typedef struct DEMO_DATA {
+    // This is the rDB pointer pack (times # of indexes for this structure) 
+    // It is required at to be at the top of a structure to be used with rDB.
+	rdb_bpp_t	pp[INDEXES]; 
+    // Below is user data
+	char 	    name[64];
+	char 	    *address_ptr;
+	uint32_t    age;
+	int64_t	    long_value;
+    char        *some_other_user_data;
+} demo_data_t;
+
+// We define a data set, and a data set pointer that we shall use later on
+demo_data_t dd,
+            *pdd;
+
+// This os the handle which is used to access and identify a data pool
+// Each data pool will have one.
+rdb_pool_t *pool;
+
+// This is a callback function. rDB will call a callback function when 
+// iterating over a data pool, once for each record.
+// This is one one perform custom operations on a big data set
+// In this case we just use it to dump the data to console.
+// ... and abord after a few records ... 
 static int myDump(void *ptr){
 	pdd=ptr;
 	printf("MyDump: %s %s %d %ld\n",
@@ -58,14 +64,14 @@ static int myDump(void *ptr){
 	return RDB_CB_OK;
 }
 
-// Insert one record, and 'dump' it to stdout, then flush tree
+// Insert one record, and dump it to stdout, then flush tree
 int simple_demo(rdb_pool_t *pool) {
 
     pdd=malloc(sizeof(demo_data_t));
     if (!pdd) fatal("fatal: failed allocating data node");
 	info("main addr %p", pdd);	
 
-    // we allocate the address pointer so it can point to a string
+    // We allocate the address pointer so it can point to a string
 	pdd->address_ptr = malloc(64);
     if (!pdd->address_ptr) fatal("fatal: failed allocating address_pointer");
     info ("address ptr = %p", pdd->address_ptr);
@@ -75,31 +81,34 @@ int simple_demo(rdb_pool_t *pool) {
 	pdd->age=43;
 	pdd->long_value=100000;
 
-    // data node is added to the tree, and Indexed.
+    // Data node is added to the tree, and Indexed.
  	if (rdb_insert(pool,pdd) != INDEXES) 
         error("one or more index failed insertion");
 
  	info("We will now dump the tree, containing one record");
     rdb_dump(pool,1);
 
-    //rDB is smart enought to know it needs to free address_ptr before it free
-    //the actual data node. (since it's an indexed field) thus helping avoid
-    //memory leaks
+    // rDB is smart enought to know it needs to free address_ptr before it free
+    // the actual data node. (since it's an indexed field) thus helping avoid
+    // memory leaks. if we had non-indexed allocated data in out structure, we
+    // would have had to use an at-delete-time callback fn which would have 
+    // freed that data. since we dont, we just use NULL's
     rdb_flush(pool, NULL, NULL);
 
-    // the pool is now empty again... ready for out next demo
-    // as such, the dump below will produce no data
+    // The pool is now empty again... ready for our next demo
+    // As such, the dump below will produce no data
     rdb_dump(pool,0);
 
     return 0;
 }
 
+// How much data we want ... we get LOOPS to the power of 4.
+// Default (18) will produce appx 100K records.
 #define LOOPS 18
 
-// here we will insert 104976 records to the tree, with 4 indexes each
+// Here we will insert 104976 records to the tree, with 4 indexes each
 int um_multi_record_insert_demo(rdb_pool_t *pool) {
 	int rc,a,b,c,d;
-	int round=0;
 	
     for (a=0; a<LOOPS; a++) {
 		for (b=0; b<LOOPS; b++) {
@@ -109,9 +118,6 @@ int um_multi_record_insert_demo(rdb_pool_t *pool) {
 					if (pdd==NULL) fatal("Allocation error in %s",__FUNCTION__);
 					pdd->address_ptr = calloc(1,16);
 					if (pdd->address_ptr == NULL) fatal("Allocation error in %s",__FUNCTION__);
-//					strcpy(pdd->name,"nnnn");
-//					strcpy(pdd->string,"+nnnn+");
-//					strcpy(pdd->pstring,"-nnnn-");
 					pdd->name[0]='A' + d;
 					pdd->name[1]='A' + c;
 					pdd->name[2]='A' + b;
@@ -121,27 +127,34 @@ int um_multi_record_insert_demo(rdb_pool_t *pool) {
                     *(pdd->address_ptr+1)='a' + c;
                     *(pdd->address_ptr+2)='a' + b;
 					*(pdd->address_ptr+3)='a' + a;
+                    // We could have calculated the various LOOPS powers out
+                    // of this loop, to increase perfoemance, but not an issue
+                    // for this demo
 					pdd->age= d + (c * LOOPS) + (b * pow(LOOPS, 2)) + (a * pow(LOOPS, 3)) ;
 					pdd->long_value=pow(LOOPS, 4) - pdd->age;
 					rc=rdb_insert(pool,pdd);
-					if (rc!=4) fatal ("%s: INSERT rc=%d (round %d) %s",__FUNCTION__ ,rc ,round, rdb_error_string);
+                    // We check that rDB was able to link all indexes. rDB will
+                    // simply skip indexes it can not link-in (due to 
+                    // duplicates, for example)
+					if (rc!=INDEXES) fatal ("%s: INSERT rc=%d %s",__FUNCTION__ , rc, rdb_error_string);
 				}
 			}
 		}
 	}
-    info ("we Just completed 400K insertions (100K records  * 4 indexes)");
+    info ("we Just completed %.0f insertions (%.0f records  * 4 indexes)", pow(LOOPS,4)*4, pow(LOOPS,4));
 
     return 0;
 }
 
 int main(int argc, char *argv[]) {
 
-//	int rc,cnt,a,b,c,d;
-//	int round=0;
-
 	printf("rDB DEMO Client\n");
+    
+    // Getting things started...
 	rdb_init();
-//	poolId1=rdbRegisterPool("demoData", INDEXES, 4, RDB_KSTR | RDB_KASC | RDB_BTREE);
+
+    // Register the data pool. (_um for un-managed pool, which is it for now)
+    // Note that the first index is also registered in this call.
 	pool = rdb_register_um_pool("demoData", 
                             INDEXES, 
                             0, //(void *) &dd.age - (void *) dd.name,
@@ -149,6 +162,7 @@ int main(int argc, char *argv[]) {
                             NULL);
     if (pool == NULL) fatal("pool registration failed");
 
+    // Registering the 3 other indexes for out data structure.
 	if (rdb_register_um_idx(pool,
                             1, 
                             (void *) &dd.address_ptr- (void *) &dd.name,
@@ -168,56 +182,72 @@ int main(int argc, char *argv[]) {
                             NULL) == -1) fatal("Failed to register index 3\n");
 
 
-//	poolId2=rdbRegisterPool("demoData2",  1, 12, RDB_KSTR | RDB_KASC | RDB_BTREE);
-//	poolId3=rdbRegisterPool("demoData3",  1, 24, RDB_KINT | RDB_KASC | RDB_BTREE);
-//	poolId4=rdbRegisterPool("demoData4",  1, 28, RDB_KLONG | RDB_KASC | RDB_BTREE);
-
-
     simple_demo(pool);
     
     um_multi_record_insert_demo(pool);
 
-    // dump ordered by name
-    //rdb_dump(pool,0);
-    // dump ordered by address, age and value
-    //rdb_dump(pool,1);
-    //rdb_dump(pool,2);
-    //rdb_dump(pool,3);
-	
+    // Uncommect Below to see the whole (long) output
+    //
+    //  Dump ordered by name
+    // rdb_dump(pool,0);
+    //
+    //  Dump ordered by address, age and value...
+    // rdb_dump(pool,1);
+    // rdb_dump(pool,2);
+    // rdb_dump(pool,3);
+
+    // Get and Print some records	
     pdd=rdb_get(pool,0,"AAAA");
-	if (pdd!=NULL) info("get: %s %s %d %ld",pdd->name, pdd->address_ptr,pdd->age,pdd->long_value);
+	if (pdd!=NULL) info("get: %s %s %d %ld",pdd->name, pdd->address_ptr,
+            pdd->age, pdd->long_value);
     pdd=rdb_get(pool,0,"AAAB");
-	if (pdd!=NULL) info("get: %s %s %d %ld",pdd->name, pdd->address_ptr,pdd->age,pdd->long_value);
+	if (pdd!=NULL) info("get: %s %s %d %ld",pdd->name, pdd->address_ptr,
+            pdd->age, pdd->long_value);
     pdd=rdb_get(pool,0,"BAAA");
-	if (pdd!=NULL) info("get: %s %s %d %ld",pdd->name, pdd->address_ptr,pdd->age,pdd->long_value);
+	if (pdd!=NULL) info("get: %s %s %d %ld",pdd->name, pdd->address_ptr,
+            pdd->age, pdd->long_value);
     pdd=rdb_get(pool,0,"BAAB");
-	if (pdd!=NULL) info("get: %s %s %d %ld",pdd->name, pdd->address_ptr,pdd->age,pdd->long_value);
+	if (pdd!=NULL) info("get: %s %s %d %ld",pdd->name, pdd->address_ptr,
+            pdd->age, pdd->long_value);
 	pdd=rdb_get(pool,1,"dead");
 	if (pdd!=NULL) {
-        info("get: %s %s %d %ld",pdd->name, pdd->address_ptr,pdd->age,pdd->long_value);
+        info("get: %s %s %d %ld",pdd->name, pdd->address_ptr,
+            pdd->age, pdd->long_value);
     }
      else info ("filed to find record with value 'dead' in index 1");
     
-    int xxx=500;	
-	pdd=rdb_get(pool,3,&xxx);
-	if (pdd!=NULL) info("get: %s %s %d %ld",pdd->name, pdd->address_ptr,pdd->age,pdd->long_value);
+    // This shows the common way to search for data ... 
+    // one use rdb_get with a pointer to a variable containing what is 
+    // search for.
+    int find_me = 500;	
+	pdd=rdb_get(pool,3,&find_me);
+	if (pdd!=NULL) info("get: %s %s %d %ld",pdd->name, pdd->address_ptr,
+            pdd->age, pdd->long_value);
 	
+    // But, at times we need a constant value. This call will allow that.
+    // (But for real numbers only, obviously).
     pdd=rdb_get_const(pool,3,501);
-	if (pdd!=NULL) info("get: %s %s %d %ld",pdd->name, pdd->address_ptr,pdd->age,pdd->long_value);
+	if (pdd!=NULL) info("get: %s %s %d %ld",pdd->name, pdd->address_ptr,
+            pdd->age, pdd->long_value);
    
-    // This is a violation (string is refferance by ponter), compiler will
+    // Below is a violation (string is refferance by ponter), compiler will
     // complain, rightfully, but it will work. One shuld use rdb_get() 
     // for strings, as done above 
     //
     // pdd=rdb_get_const(pool,1,"abcd");
 	// if (pdd!=NULL) info("get: %s %s %d %ld",pdd->name, pdd->address_ptr,pdd->age,pdd->long_value);
     
-
     // This will iterate the entire data pool, calling myDump on each record
     // BUT, return code from the call can intervene. in this case, it will
-    // about the cycle after print record where age==3	
-    // try changing the index we use to iterate on . what happened?
+    // abort the cycle after printing the record where at age == 3	
+    // Try changing the index we use to iterate on . what happened?
     rdb_iterate(pool,2,(void *) &myDump, NULL, NULL, NULL);
+    
+    // free all data in the tree
+    rdb_flush(pool, NULL, NULL);
+
+    //Clean out rdb leftovers (like pools data), free all resources.
+    rdb_clean();
 
     exit(0);
 }
