a4adae5bf7e07d19d99848feebfffa88e4c45073
Assaf Stoler <assaf@vantagerobotics.com>
2018-08-06 14:39:20 -0700

    Auto-generated Enums for rdbfw-messaging to enable (con)textual debug prints (strings)

--- rdbfw/messaging.h
+++ rdbfw/messaging.h
@@ -9,49 +9,84 @@
 // Note: The following 4 enum's does not overlap.
 // This was done to allow delivery speed optimizations.
 // It is no longer needed so overlap is OK, but i left it as is as I see no reason to change.
-typedef enum {
-    RDBMSG_ROUTE_NA=0,
-    RDBMSG_ROUTE_FW,
-    RDBMSG_ROUTE_MDL_PINGPONG,
-    RDBMSG_ROUTE_MDL_SOCKEER_COMM,
-    RDBMSG_ROUTE_MDL_TIMERS,
-    RDBMSG_ROUTE_MDL_TIMED,
-    RDBMSG_ROUTE_MDL_EVENT_SKEL,
-    RDBMSG_ROUTE_MAX=1023,
-} rdbmsg_routing_e ;
 
-typedef enum {
-    RDBMSG_GROUP_NA=2048,
-    RDBMSG_GROUP_TIMERS,
-    RDBMSG_GROUP_LOGGING,
-    RDBMSG_GROUP_MAX=3095,
+#ifdef __GNUC__
+#define NO_UNUSED_WARNING __attribute__ ((unused))
+#else
+#define NO_UNUSED_WARNING
+#endif
+
+#define FOREACH_ROUTE(ROUTE) \
+    ROUTE(RDBMSG_ROUTE_NA=0) \
+    ROUTE(RDBMSG_ROUTE_FW) \
+    ROUTE(RDBMSG_ROUTE_MDL_PINGPONG) \
+    ROUTE(RDBMSG_ROUTE_MDL_SOCKEER_COMM) \
+    ROUTE(RDBMSG_ROUTE_MDL_TIMERS) \
+    ROUTE(RDBMSG_ROUTE_MDL_TIMED) \
+    ROUTE(RDBMSG_ROUTE_MDL_EVENT_SKEL) \
+    ROUTE(RDBMSG_ROUTE_MAX=1023) \
+
+#define GENERATE_ENUM(ENUM) ENUM,
+#define GENERATE_STRING(STRING) #STRING,
+
+typedef enum ROUTE_ENUM {
+    FOREACH_ROUTE(GENERATE_ENUM)
+} rdbmsg_routing_e;
+
+static const char NO_UNUSED_WARNING *ROUTE_STRING[] = {
+    FOREACH_ROUTE(GENERATE_STRING)
+};
+
+
+#define FOREACH_GROUP(GROUP) \
+    GROUP(RDBMSG_GROUP_NA=0) \
+    GROUP(RDBMSG_GROUP_TIMERS) \
+    GROUP(RDBMSG_GROUP_LOGGING) \
+    GROUP(RDBMSG_GROUP_MAX=3095) \
+
+typedef enum GROUP_ENUM {
+    FOREACH_GROUP(GENERATE_ENUM)
 } rdbmsg_group_e;
 
-typedef enum {
-    RDBMSG_ID_NA=3096,
-    RDBMSG_ID_TIMER_START,
-    RDBMSG_ID_TIMER_STOP,
-    RDBMSG_ID_TIMER_ACK,
-    RDBMSG_ID_TIMER_TICK_0,
-    RDBMSG_ID_TIMER_TICK_1,
-    RDBMSG_ID_TIMER_TICK_2,
-    RDBMSG_ID_TIMER_TICK_3,
-    RDBMSG_ID_TIMER_TICK_4,
-    RDBMSG_ID_TIMER_TICK_5,
-    RDBMSG_ID_TIMER_TICK_6,
-    RDBMSG_ID_TIMER_TICK_7,
-    RDBMSG_ID_TIMER_TICK_8,
-    RDBMSG_ID_TIMER_TICK_9,
-    RDBMSG_ID_TIMER_TICK_10,
-    RDBMSG_ID_TIMER_TICK_11,
-    RDBMSG_ID_TIMER_TICK_12,
-    RDBMSG_ID_TIMER_TICK_13,
-    RDBMSG_ID_TIMER_TICK_14,
-    RDBMSG_ID_TIMER_TICK_15,
-    RDBMSG_ID_LAST_GASP,
-    RDBMSG_ID_MAX=65535,
+static const char NO_UNUSED_WARNING *GROUP_STRING[] = {
+    FOREACH_GROUP(GENERATE_STRING)
+};
+
+#define FOREACH_ID(ID) \
+    ID(RDBMSG_ID_NA=3096) \
+    ID(RDBMSG_ID_TIMER_START) \
+    ID(RDBMSG_ID_TIMER_STOP) \
+    ID(RDBMSG_ID_TIMER_ACK) \
+    ID(RDBMSG_ID_TIMER_TICK_0) \
+    ID(RDBMSG_ID_TIMER_TICK_1) \
+    ID(RDBMSG_ID_TIMER_TICK_2) \
+    ID(RDBMSG_ID_TIMER_TICK_3) \
+    ID(RDBMSG_ID_TIMER_TICK_4) \
+    ID(RDBMSG_ID_TIMER_TICK_5) \
+    ID(RDBMSG_ID_TIMER_TICK_6) \
+    ID(RDBMSG_ID_TIMER_TICK_7) \
+    ID(RDBMSG_ID_TIMER_TICK_8) \
+    ID(RDBMSG_ID_TIMER_TICK_9) \
+    ID(RDBMSG_ID_TIMER_TICK_10) \
+    ID(RDBMSG_ID_TIMER_TICK_11) \
+    ID(RDBMSG_ID_TIMER_TICK_12) \
+    ID(RDBMSG_ID_TIMER_TICK_13) \
+    ID(RDBMSG_ID_TIMER_TICK_14) \
+    ID(RDBMSG_ID_TIMER_TICK_15) \
+    ID(RDBMSG_ID_LAST_GASP) \
+    ID(RDBMSG_ID_MUX_STATE) \
+    ID(RDBMSG_ID_MAX=65535) \
+
+    // SAPI will replace individual status messages. Id's above remain to maintain correct sequance ID's
+
+
+typedef enum ID_ENUM {
+    FOREACH_ID(GENERATE_ENUM)
 } rdbmsg_id_e;
 
+static const char NO_UNUSED_WARNING *ID_STRING[] = {
+    FOREACH_ID(GENERATE_STRING)
+};
 
 //The following two structures must stay in sync for the overlapping portion.
 typedef struct rdbmsg_msg_s {
@@ -114,8 +149,10 @@ void rdbmsg_register_hooks(void);
 int rdbmsg_init(rdb_pool_t *plugin_pool);
 int rdbmsg_request(void  *ctx, int from, int to, int group, int id);
 int rdbmsg_emit_simple(int from, int to, int group, int id, int data);
+int rdbmsg_emit (int from, int to, int group, int id, int length, void *data);
 int rdbmsg_delay_HZ(int new_Hz);
 int rdbmsg_free (plugins_t *ctx, rdbmsg_queue_t *q);
 void rdbmsg_destroy_tree (void *data, void *user_ptr, int stage);
+void rdbmsg_clean(void);
 
 #endif
