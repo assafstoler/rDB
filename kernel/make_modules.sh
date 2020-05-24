029e753f8434608b33c264d6db59868b127dedc6
Assaf Stoler <assaf@vantagerobotics.com>
2020-05-24 06:09:03 -0700

    Only try to build KM if kernel headers are available

--- kernel/make_modules.sh
+++ kernel/make_modules.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-make -C /lib/modules/`uname -r`/build M=`pwd` $1
+if [ -d /lib/modules/`uname -r`/build ] ; then
+    echo Building KM $1 -
+    make -C /lib/modules/`uname -r`/build M=`pwd` $1
+fi
